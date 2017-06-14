f0488f3290bdb1a52fb3767c8764f936d51d5559
Gen Ichino <zenoplex@gmail.com>
2017-06-15 02:22:13 +0900

    feat: add SetFormAction

--- src/modules/form/actions.ts
+++ src/modules/form/actions.ts
@@ -1,12 +1,27 @@
 import { Action } from 'redux';
 
 export const SUBMIT = 'form/SUBMIT';
+export const SET_FORM = 'form/SET_FORM';
+
+export interface FormData {
+  lastName: string,
+  firstName: string,
+}
 
 export interface SubmitAction extends Action {
   type: typeof SUBMIT;
+  data: FormData;
 }
 
-export type FormActions = SubmitAction;
+export interface SetFormAction extends Action {
+  type: typeof SET_FORM;
+  data: FormData;
+}
 
-export const submit: () => SubmitAction
-= () => ({ type: SUBMIT });
+export type FormActions = SubmitAction | SetFormAction;
+
+export const submit: (_: FormData) => SubmitAction
+= (data) => ({ type: SUBMIT, data });
+
+export const setFormAction: (_: FormData) => SetFormAction
+= (data) => ({ type: SET_FORM, data });
