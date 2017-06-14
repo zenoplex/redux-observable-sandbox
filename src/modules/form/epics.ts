0f934240cb57ba697b6eb14c4760b7cfef6b5cbd
Gen Ichino <zenoplex@gmail.com>
2017-06-15 02:23:32 +0900

    feat: add epic

--- src/modules/form/epics.ts
+++ src/modules/form/epics.ts
@@ -1,15 +1,29 @@
 import 'rxjs'
 import { combineEpics, Epic } from 'redux-observable';
-import { SUBMIT, SubmitAction } from './actions';
+import { startSubmit, stopSubmit } from 'redux-form';
+import { SUBMIT, SET_FORM, setFormAction, SubmitAction, SetFormAction } from './actions';
 import { ReduxState } from '../../store';
 
-const submitEpic: Epic<SubmitAction, ReduxState>
+const submitEpic: Epic<SubmitAction | SetFormAction, ReduxState>
 = action$ => action$
   .ofType(SUBMIT)
+  .delay(5000)
   .do(console.log)
-  .mapTo({ type: 'TEST' })
+  .map(action => setFormAction(action.data))
 
+const startEpic: Epic<SubmitAction, ReduxState>
+= action$ => action$
+  .ofType(SUBMIT)
+  .mapTo(startSubmit('name'));
+
+const setFormEpic: Epic<SetFormAction, ReduxState>
+= action$ => action$
+  .ofType(SET_FORM)
+  .do(console.log)
+  .mapTo(stopSubmit('name'));
 
 export const epics = combineEpics(
   submitEpic,
+  startEpic,
+  setFormEpic,
 );
